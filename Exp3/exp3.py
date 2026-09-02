f4953c6e276a9fb71c4d9995972be936513e22ad
HANSTS13 <hanstomsojan2028@cs.sjcetpalai.ac.in>
2026-09-02 11:21:48 +0530

    Update exp3.py

--- Exp3/exp3.py
+++ Exp3/exp3.py
@@ -24,3 +24,36 @@ print("Dataset Shape:",x.shape)
 print("Classes:",np.unique(y))
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42,stratify =y)
+
+scaler=StandardScaler()
+x_train=scaler.fit_transform(x_train)
+x_test=scaler.transform(x_test)
+
+mle_model= LogisticRegression(
+    penalty=None,
+    max_iter=5000,
+    random_state=42
+)
+mle_model.fit(x_train,y_train)
+y_pred_mle=mle_model.predict(x_test)
+
+map_l2=LogisticRegression(
+    penalty='l2',
+    C=1.0,
+    solver='lbfgs',
+    max_iter=5000,
+    random_state=42 
+    )
+map_l2.fit(x_train,y_train)
+y_pred_l2=map_l2.predict(x_test)
+
+map_l1=LogisticRegression(
+    penalty='l1',
+    C=1.0,
+    solver='liblinear',
+    max_iter=5000,
+    random_state=42 
+    )
+map_l1.fit(x_train,y_train)
+y_pred_l1=map_l1.predict(x_test)
+
